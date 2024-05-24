f05c20e25ffbca73a41b2d3db3a77b3f0ccdd46b
Juanma Oro <juanorodiaz96@gmail.com>
2024-05-23 21:21:51 -0300

    realiamos un servidor web donde servimos una red social como la del pajarito

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-const { envs } = require('./config/env')
+const { envs } = require('./config/env') 
+const{ startServer } = require('./server/server')
 
 
 
 const main =() => {
-    console.log(envs)
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+
+    })
 }
 
 //funcion agnostica(no tiene nombre)
